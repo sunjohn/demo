433d34ddcd121c8cd0375b7acb5c99f57c981f13
Sunjohn <516689667@qq.com>
2012-07-21 21:03:22 +0800

    redirect link

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Demo::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
 
-  get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
 
   
   # The priority is based upon order of creation:
@@ -58,7 +58,7 @@ Demo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+ 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
