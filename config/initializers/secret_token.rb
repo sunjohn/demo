edade0866ddd3fe642e1a59a821d7275697794e6
sunjohn <sunjohn@sunjohn.(none)>
2012-07-21 14:08:58 +0800

    Improve the READERME

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '28d3859e6f34854f3631bf2df95ecb5c2265f394b826f8d4922dc66f667603f94ba6025d89b0dcdb940b79d60d0b23ed3e7c254dd93e58a978b3056614248199'
